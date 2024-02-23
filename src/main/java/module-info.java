152d214b23a5e2a6ca386e6b38effd5e3a51f888
yosra haboubi <yosrahaboubi27@gmail.com>
2024-02-23 23:09:15 +0100

    operations crud utilisateur

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module tn.esprit.gestionvols {
     requires javafx.fxml;
     requires java.sql;
     requires org.controlsfx.controls;
+    requires mysql.connector.j;
 
     opens tn.esprit.gestionvols to javafx.fxml;
     exports tn.esprit.gestionvols;
--- src/main/java/tn/esprit/gestionvols/Main.java
+++ src/main/java/tn/esprit/gestionvols/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tn.esprit.gestionvols.Services.ServiceUtilisateur;
 import tn.esprit.gestionvols.Utilities.SingletonConnexion;
 
 import java.io.IOException;
@@ -11,27 +12,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
-    /*@Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }*/
-
     public static void main(String[] args) {
-       // launch();
+        ServiceUtilisateur s=new ServiceUtilisateur();
+        System.out.println(s.AfficherUtilisateurById(9));
 
-        /* creation instance de la connexion
-        SingletonConnexion dbConnection = SingletonConnexion.getInstance();
-        Connection conn = dbConnection.getConnection();
-
-        // Fermeture de la connexion à la base de données
-        try {
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
     }
 }
